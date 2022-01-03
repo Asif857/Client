aec98d7096d6b711937a8fb2902129e96b1179e9
nadavyaf@wincs.cs.bgu.ac.il <nadavy13@gmail.com>
2022-01-03 19:20:51 +0200

    Finished bidiProtocol

--- cmake-build-debug/CMakeFiles/SPL3new.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/SPL3new.dir/cmake_clean.cmake
@@ -1,6 +1,7 @@
 file(REMOVE_RECURSE
   "CMakeFiles/SPL3new.dir/main.cpp.o"
   "CMakeFiles/SPL3new.dir/src/Task.cpp.o"
+  "CMakeFiles/SPL3new.dir/src/bidiProtocol.cpp.o"
   "CMakeFiles/SPL3new.dir/src/encoderDecoder.cpp.o"
   "SPL3new"
   "SPL3new.pdb"
